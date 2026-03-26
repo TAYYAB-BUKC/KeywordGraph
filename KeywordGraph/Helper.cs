01fa627f2da870a90a9b70fbf3ae61ca70427013
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-03-27 03:49:59 +0500

    GetEnvironmentVariable implemented in Helper.

---
+++ KeywordGraph/Helper.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace KeywordGraph;
+
+public class Helper
+{
+    public static string GetEnvironmentVariable(string key)
+    {
+        var value = Environment.GetEnvironmentVariable(key);
+        if (string.IsNullOrWhiteSpace(value))
+            throw new Exception($"Missing Environment Variable: {key}");
+        return value!;
+    }
+}
