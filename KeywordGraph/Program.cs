e67c2724e52c8d45845d52fbf751431eb7f2d9ca
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-03-28 01:31:06 +0500

    Attempt 1 implemented successfully.

--- KeywordGraph/Program.cs
+++ KeywordGraph/Program.cs
@@ -1,9 +1,45 @@
 ﻿// set up the client
 
-using KeywordGraph;
+// using KeywordGraph;
 
-var words = new[] { "cat", "mouse", "lion", "tiger", "helicopter", "train", "blue", "carrot", "space" };
+// var words = new[] { "cat", "mouse", "lion", "tiger", "helicopter", "train", "blue", "carrot", "space" };
 
-await Helper.GenerateEmbeddings(words);
+// await Helper.GenerateEmbeddings(words);
+
+#region Attempt 1
+using Microsoft.Extensions.AI;
+using OllamaSharp;
+using OllamaSharp.Models.Chat;
+
+var uri = new Uri("http://localhost:11434");
+var ollama = new OllamaApiClient(uri);
+ollama.SelectedModel = "qwen3-embedding";
+var messages = new List<Message>();
+
+messages.Add(new Message()
+{
+    Role = OllamaSharp.Models.Chat.ChatRole.System,
+    Content = "You are a helpful assistant"
+});
+
+
+messages.Add(new Message()
+{
+    Role = OllamaSharp.Models.Chat.ChatRole.User,
+    Content = "wht is 1 +1?"
+});
+
+
+var request = new OllamaSharp.Models.Chat.ChatRequest()
+{
+    Model = "qwen3:4b",
+    Messages = messages
+};
+await foreach (var response in ollama.ChatAsync(request))
+{
+    System.Console.WriteLine(response);
+}
+Console.ReadKey();
+#endregion
 
 Console.ReadKey();
