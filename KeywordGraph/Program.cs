a064e96f244d3ebb6e8167f64fb747a1e78fb620
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-03-27 23:48:56 +0500

    GenerateEmbeddings method tested in Program.

--- KeywordGraph/Program.cs
+++ KeywordGraph/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// set up the client
+
+using KeywordGraph;
+
+var words = new[] { "cat", "mouse", "lion", "tiger", "helicopter", "train", "blue", "carrot", "space" };
+
+await Helper.GenerateEmbeddings(words);
+
+Console.ReadKey();
